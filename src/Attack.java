b15b5991e44fafa2682f2d3d8699fc44879e6f14
Jeff Gouge <gouge.jeff@gmail.com>
2015-02-12 22:37:51 -0500

    GuessCacheSize complete.

--- src/Attack.java
+++ src/Attack.java
@@ -16,4 +16,51 @@ public class Attack {
         Node target = custodians.get(rand.nextInt(custodians.size()));
         return target;
     }
+
+    public static int GuessCacheSize(Node attacker, Node target, List<Content> K, int cacheSizeGuess){
+
+        int SumCacheHits = 0;
+        for(Content distinctFile : K)
+        {
+            Packet p = new Packet(attacker,distinctFile);
+            p.dest = attacker.contentCustodians.get(distinctFile);
+            p.route = Dijkstra.getShortestPath(p.src,p.dest);
+            Packet r = Search.findContent(p);
+            if(r.cachehit)
+                SumCacheHits++;
+        }
+
+        Collections.reverse(K);
+
+        for(Content distinctFile : K)
+        {
+            Packet p = new Packet(attacker,distinctFile);
+            p.dest = attacker.contentCustodians.get(distinctFile);
+            p.route = Dijkstra.getShortestPath(p.src,p.dest);
+            Packet r = Search.findContent(p);
+            if(r.cachehit)
+                SumCacheHits++;
+        }
+
+        Collections.reverse(K);
+
+        if(SumCacheHits == cacheSizeGuess)
+        {
+            SumCacheHits = 2* SumCacheHits;
+            GuessCacheSize(attacker, target, K, SumCacheHits);
+        }else{
+            return SumCacheHits;
+        }
+
+        return 1;
+    }
+
+    public static int GuessCharacteristicTime(Node target, int CacheSizeGuess) {
+        return 1;
+    }
+
+    public static void RunAttack(Node attacker, Node target){
+
+    }
+
 }
