040150f66d115cf18c37723beff6d5f61cd8c5fb
Jeff <gouge.jeff@gmail.com>
2015-02-23 23:47:14 -0500

    Update for issues with attacker node.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import javax.swing.*;
-import java.awt.*;
 import java.util.*;
-import org.jfree.*;
+
 
 
 public class Main {
 
-    public static void main(String[] args) throws java.io.IOException, java.lang.Exception {
+    public static void main(String[] args) throws java.lang.Exception {
 
         //Create an arraylist of all tests, and results
         ArrayList<ArrayList<PacketTracer>> allTests = new ArrayList<ArrayList<PacketTracer>>();
@@ -15,12 +13,12 @@ public class Main {
 
         //Run a test(graph, number of requests to perform, cache enabled, number of attackers)
         //To change the number of tests change the integer for testsize
-        int testsize = 10;
+        int testsize = 50;
         double alpha = 1;
-        int numAttackers = 2;
+        int numAttackers = 1;
 
         //1 = LRU, 2 = FIFO, 3=Random
-        int cacheType = 1;
+        int cacheType;
 
         //Loop for number of cache types
         for(int c = 1; c < 4; c++) {
@@ -37,12 +35,12 @@ public class Main {
                 if (y == 0) {
                     for (int n = 0; n < testsize; n++)
                         //Run without cache
-                        tests.add(Search.runTest(g, 100, false, numAttackers));
+                        tests.add(Search.runTest(g, 5000, false, numAttackers));
 
                 } else {
                     for (int x = 0; x < testsize; x++) {
                         //Run with cache. Increases by 10. Run 100000 tests
-                        tests.add(Search.runTest(g, 100, true, numAttackers));
+                        tests.add(Search.runTest(g, 5000, true, numAttackers));
                     }//end for number of tests
                 }//end else
             }//end for cachesize tests
