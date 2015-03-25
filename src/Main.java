4483e4500dea9cfde82d4cce7ffcac2de46a04cc
Jeff <gouge.jeff@gmail.com>
2015-03-24 23:25:26 -0400

    Update for sending attacks in bulk

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,13 @@ public class Main {
 
         //Run a test(graph, number of requests to perform, cache enabled, number of attackers)
         //To change the number of tests change the integer for testsize
-        int testsize = 20;
-        int requestsPerTest = 100000;
+        int testsize = 5;
+        int requestsPerTest = 200000;
         double poissonRate = .65;
         double zipfianAlpha = .65;
-        int numAttackers = 1;
-        int numUnpopularItems = 20;
-        int numContentItems = 250;
+        int numAttackers = 0;
+        int numUnpopularItems = 50;
+        int numContentItems = 1000;
 
         //1 = LRU, 2 = FIFO, 3=Random
         int cacheType;
@@ -35,7 +35,7 @@ public class Main {
                 if (y == 0) {
                     for (int n = 0; n < testsize; n++) {
                         //Create square graph(x,y,cacheSize,alpha, cacheType, numAttackers, numUnpopularItems, numContentItems)
-                        g = new Graph(5, 5, y * 10, zipfianAlpha, cacheType, numAttackers, numUnpopularItems, numContentItems, requestsPerTest);
+                        g = new Graph(10, 10, y * 10, zipfianAlpha, cacheType, numAttackers, numUnpopularItems, numContentItems, requestsPerTest);
                         g.createGraph();
                         //Run without cache
                         System.out.println("Test Number: " +n);
@@ -45,7 +45,7 @@ public class Main {
                 } else {
                     for (int x = 0; x < testsize; x++) {
                         //Create square graph(x,y,cacheSize,alpha, cacheType, numAttackers, numUnpopularItems, numContentItems)
-                        g = new Graph(5, 5, y * 10, zipfianAlpha, cacheType, numAttackers, numUnpopularItems, numContentItems, requestsPerTest);
+                        g = new Graph(10, 10, y * 10, zipfianAlpha, cacheType, numAttackers, numUnpopularItems, numContentItems, requestsPerTest);
                         g.createGraph();
                         //Run with cache.
                         System.out.println("Test Number: " +x);
